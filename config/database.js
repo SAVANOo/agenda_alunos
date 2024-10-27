444d0ea8fc59692c77220baf0a9a8b4755d1fd15
SAVANOo <savio_gato1000@hotmail.com>
2024-10-26 23:02:57 -0300

    Criando conexão com banco de dados

---
+++ config/database.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+pool.connect()
+    .then(() => console.log('Conectado ao banco de dados!'))
+    .catch(err => console.error('Erro ao conectar ao banco de dados', err.stack));
+
+module.exports = pool;
