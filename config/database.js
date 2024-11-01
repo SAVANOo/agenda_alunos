9c7e56bf7b882131924ef46e781ae400bcd36e9b
SAVANOo <savio_gato1000@hotmail.com>
2024-10-31 21:06:01 -0300

    Criando método para retentativas de conexão

--- config/database.js
+++ config/database.js
@@ -9,8 +9,15 @@ const pool = new Pool({
     port: process.env.DB_PORT,
 });
 
-pool.connect()
-    .then(() => console.log('Conectado ao banco de dados!'))
-    .catch(err => console.error('Erro ao conectar ao banco de dados', err.stack));
+function connectWithRetry() {
+    pool.connect()
+        .then(() => console.log('Conectado ao banco de dados!'))
+        .catch(err => {
+            console.error('Erro ao conectar ao banco de dados', err.stack)
+            console.error('Tentando reconectar ao banco em 3 minutos...')
+            setTimeout(connectWithRetry, 3 * 60 * 1000);
+        });
+}
 
+connectWithRetry();
 module.exports = pool;
